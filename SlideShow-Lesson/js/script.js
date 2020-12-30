7812742d70a566c54b40675039829c87bd701fb4
Motoyuki Koseki <motoyuki.koseki@gmail.com>
2020-12-30 15:38:12 +0900

    [Add]JavaScriptの練習

---
+++ SlideShow-Lesson/js/script.js
@@ -0,0 +1,12 @@
+$(".theTarget").skippr({
+  transition : "fade",
+  speed : 800,
+  easing : "easeOutQuart",
+  navType : "bubble",
+  childrenElementType : "div",
+  arrows : true,
+  autoPlay : false,
+  autoPlayDuration : 5000,
+  keyboardOnAlways : true,
+  hidePrevious : false
+});
